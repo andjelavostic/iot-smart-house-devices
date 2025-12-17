8a282bbbda2f6ff24670321335cb1a246fe9b3a6
andjelavostic <andjelavostic@gmail.com>
2025-12-17 23:43:17 +0100

    [ADD] loading settings in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from settings import load_settings
 
 try:
     import RPi.GPIO as GPIO
@@ -10,9 +11,16 @@ except:
 
 if __name__ == "__main__":
     print('Starting app')
+    settings = load_settings()
     threads = []
     stop_event = threading.Event()
     try:
+        ds1_settings = settings['DS1']
+        dl_settings = settings['DL']
+        dus1_settings = settings['DUS1']
+        db_settings = settings['DB']
+        dpir1_settings = settings['DPIR1']
+        dms_settings = settings['DMS']
         while True:
             time.sleep(1)
 
