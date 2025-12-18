e5aa19d8a75b9410396f2c81cc8b417a49967bcf
andjelavostic <andjelavostic@gmail.com>
2025-12-18 17:19:23 +0100

    [ADD] registry for actuators

--- registry.py
+++ registry.py
@@ -1,3 +1,4 @@
+from simulators import led_sim
 from simulators.button_sim import button_sim
 from simulators.pir_sim import pir_sim
 from simulators.membrane_switch_sim import ms_sim
@@ -7,4 +8,7 @@ SENSOR_REGISTRY = {
     "button": button_sim,
     "pir": pir_sim,
     "membrane": ms_sim
+}
+ACTUATOR_REGISTRY = {
+    "led": led_sim
 }
