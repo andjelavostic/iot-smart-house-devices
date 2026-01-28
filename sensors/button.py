59607d6fd9b8f80f41712ff862021125a955cf63
brankaaaaak <kovacevicbranka123@gmail.com>
2026-01-28 22:17:44 +0100

    [CREATE] button

---
+++ sensors/button.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+def run_button_real(sensor_code, settings, on_state_change, stop_event):
+    pin = settings['pin']
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    last_state = GPIO.input(pin)
+
+    while not stop_event.is_set():
+        current_state = GPIO.input(pin)
+        
+
+        is_pressed = (current_state == GPIO.LOW)
+        
+        if is_pressed != (last_state == GPIO.LOW):
+            if is_pressed:
+                print(f"[{sensor_code}] REAL Button PRESSED")
+            else:
+                print(f"[{sensor_code}] REAL Button RELEASED")
+            
+            on_state_change(sensor_code, settings, is_pressed)
+            last_state = current_state
+            
+        time.sleep(0.1) 
