938bac6d006254792ea7951c5c268d2a276bdb26
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-08 21:34:20 +0100

    [UPDATE] setting.py

--- settings.py
+++ settings.py
@@ -1,5 +1,11 @@
 import json
+import sys
 
-def load_settings(filePath='PI1-settings.json'):
-    with open(filePath, 'r') as f:
+def load_settings():
+    
+    file_name = 'PI1-settings.json'
+    if len(sys.argv) > 1:
+        file_name = f"{sys.argv[1]}-settings.json"
+        
+    with open(file_name, 'r') as f:
         return json.load(f)
