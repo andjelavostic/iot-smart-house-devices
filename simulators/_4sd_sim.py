a5792a4904dc9ba229a7b37897da3bf38b01da6a
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-08 21:35:21 +0100

    [CREATE] dht 4sd and gyroscope simulator

---
+++ simulators/_4sd_sim.py
@@ -0,0 +1,18 @@
+import time
+
+def run_4sd_simulator(actuator_code, stop_event, settings, on_state_change):
+    last_displayed_value = None
+
+    while not stop_event.is_set():
+
+        current_val = settings.get("value", time.strftime("%H:%M"))
+
+        if current_val != last_displayed_value:
+            print(f"--- DISPLEJ [{actuator_code}] ---")
+            print(f"|  {current_val}  |")
+            print("----------------------")
+            
+            on_state_change(actuator_code, settings, current_val)
+            last_displayed_value = current_val
+
+        time.sleep(1)
