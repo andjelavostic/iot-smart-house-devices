00e138199904f7b211b4fc56705553c3d4ed7b84
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-08 21:35:50 +0100

    [CREATE] PI2-settings

--- registry.py
+++ registry.py
@@ -23,8 +23,9 @@ except ImportError:
     run_buzzer_real = None
 from simulators.keyboard_sim import keyboard_sim
 from simulators.ultrasonic_sim import ultrasonic_sim
-
-
+from simulators.gyroscope_sim import gyro_simulator
+from simulators.dht_sim import dht_simulator
+from simulators._4sd_sim import run_4sd_simulator
 SENSOR_REGISTRY = {
     "button": {
         "true": run_button_real,
@@ -42,7 +43,12 @@ SENSOR_REGISTRY = {
     "ultrasonic":{
         "true":run_ultrasonic_real,
         "sim":ultrasonic_sim
-    }
+    },
+    "gyro": lambda sensor_code, delay, on_value, stop_event, settings: 
+            gyro_simulator(delay, lambda a, g, p, s: on_value(sensor_code, s, {"accel": a, "gyro": g}), stop_event, None, settings),
+    
+    "dht": lambda sensor_code, delay, on_value, stop_event, settings:
+            dht_simulator(delay, lambda h, t, p, s: on_value(sensor_code, s, {"temp": t, "hum": h}), stop_event, None, settings)
 }
 ACTUATOR_REGISTRY = {
     "led":{
@@ -52,5 +58,6 @@ ACTUATOR_REGISTRY = {
     "buzzer": {
         "true": run_buzzer_real,
         "sim": buzzer_sim
-    }
+    },
+    "7segment": run_4sd_simulator 
 }
