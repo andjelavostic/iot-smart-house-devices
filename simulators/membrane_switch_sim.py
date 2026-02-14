df3b30ff8eaa150c31bfb21e03266f85a6e0478d
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-14 13:42:36 +0100

    [UPDATE] membrane_switch_sim to periodically send the correct pin

--- simulators/membrane_switch_sim.py
+++ simulators/membrane_switch_sim.py
@@ -3,11 +3,15 @@ import random
 
 KEYS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "#", "A", "B", "C", "D"]
 
-def generate_values():
+def generate_values(correct_pin="1234", probability=0.2):
     while True:
-        length = random.randint(3, 5) 
-        code = "".join(random.choice(KEYS) for _ in range(length))
-        yield code
+        # 20% šanse da pošalje ispravan PIN
+        if random.random() < probability:
+            yield correct_pin
+        else:
+            length = random.randint(3, 5)
+            code = "".join(random.choice(KEYS) for _ in range(length))
+            yield code
 
 def ms_sim(sensor_code, delay, on_value, stop_event, settings=None):
     gen = generate_values()
