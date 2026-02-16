1d97984875e31cd1710e9d3dc84f95e236440a4e
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-16 22:03:03 +0100

    [FIX] gyroscope real and sim

--- sensors/gyro/gyro.py
+++ sensors/gyro/gyro.py
@@ -10,21 +10,28 @@ except ImportError as e:
     print(f"Greška pri uvozu MPU6050: {e}")
     MPU6050 = None
 
-def run_gyro_real(sensor_code, delay, on_value, stop_event, settings):
-    # Inicijalizacija MPU6050 objekta
+def run_gyro_real(sensor_code, delay, stop_event, on_value, settings):
+
+    if MPU6050 is None:
+        print("Greška: MPU6050 biblioteka nije instalirana.")
+        return
+
     mpu = MPU6050(1, 0x68, 0, 0, 0, 0, 0, 0, False) 
-    
     print(f"Pokrenuto čitanje sa PRAVOG žiroskopa [{sensor_code}]")
 
-    while not stop_event.is_set():
-        accel = mpu.get_acceleration()
-        gyro = mpu.get_rotation()
-        
-        val = {
-            "accel": [accel[0]/16384.0, accel[1]/16384.0, accel[2]/16384.0],
-            "gyro": [gyro[0]/131.0, gyro[1]/131.0, gyro[2]/131.0]
-        }
-        
-        on_value(sensor_code, settings, val)
-        
-        time.sleep(delay)
+    try:
+        while not stop_event.is_set():
+            accel = mpu.get_acceleration()
+            gyro = mpu.get_rotation()
+            
+            val = {
+                "accel": [accel[0]/16384.0, accel[1]/16384.0, accel[2]/16384.0],
+                "gyro": [gyro[0]/131.0, gyro[1]/131.0, gyro[2]/131.0]
+            }
+            
+            # ovde prodljedjivanje podataka mainu
+            on_value(sensor_code, settings, val)
+            
+            time.sleep(delay)
+    except Exception as e:
+        print(f"Greška u radu žiroskopa: {e}")
--- simulators/gyroscope_sim.py
+++ simulators/gyroscope_sim.py
@@ -1,9 +1,7 @@
 import time
 import random
 
-
 max_raw = 32767
-
 
 def generate_values():
     accel = [random.randint(-max_raw, max_raw) for _ in range(3)]
@@ -12,10 +10,17 @@ def generate_values():
     gyro_scaled = [g / 131.0 for g in gyro]
     return accel_scaled, gyro_scaled
 
-def gyro_simulator(delay, callback, stop_event, publish_event, settings):
-    while True:
+def gyro_simulator(sensor_code, delay, stop_event, on_value, settings):
+
+    while not stop_event.is_set():
         accel, gyro = generate_values()
-        callback(accel, gyro, publish_event, settings)
-        if stop_event.is_set():
-            break
+        
+        val = {
+            "accel": accel,
+            "gyro": gyro
+        }
+        
+        # pozivamo on_value (što je lambda u mainu) sa tačno 3 argumenta
+        on_value(sensor_code, settings, val)
+        
         time.sleep(delay)
