f1890f9b0fe50da50c1cfc5da8ebcb2a1767c95c
brankaaaaak <kovacevicbranka123@gmail.com>
2026-02-16 22:02:14 +0100

    [FIX] dth simulator

--- simulators/dht_sim.py
+++ simulators/dht_sim.py
@@ -15,9 +15,17 @@ def generate_values(initial_temp=25, initial_humidity=20):
         yield humidity, temperature
 
 
-def dht_simulator(delay, callback, stop_event, publish_event, settings):
+# def dht_simulator(delay, callback, stop_event, publish_event, settings):
+#     for h, t in generate_values():
+#         time.sleep(delay)  
+#         callback(h, t, publish_event, settings)
+#         if stop_event.is_set():
+#             break
+
+def dht_simulator(sensor_code, delay, stop_event, on_value, settings): 
     for h, t in generate_values():
-        time.sleep(delay)  
-        callback(h, t, publish_event, settings)
         if stop_event.is_set():
             break
+        time.sleep(delay)
+        val = {"temperature": t, "humidity": h}
+        on_value(sensor_code, settings, val)
